9ca91306e4746760c5526f9ea9376416d8699cb3
Ryne Cheow <rynecheow@gmail.com>
2020-07-13 08:38:01 +0800

    chore: Cleanup
    Signed-off-by: Ryne Cheow <rynecheow@gmail.com>

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,3 @@
-swiftformat App/App --config ./.swiftformat --quiet
-swiftformat App/AppTests --config ./.swiftformat --quiet
-swiftformat App/Notification\ Service\ Extension --config ./.swiftformat --quiet
-swiftformat App/Shared --config ./.swiftformat --quiet
-swiftformat App/Widget --config ./.swiftformat --quiet
-swiftformat Domain/Domain --config ./.swiftformat --quiet
-swiftformat Domain/DomainTests --config ./.swiftformat --quiet
-swiftformat Data/Data --config ./.swiftformat --quiet
-swiftformat Data/DataTests --config ./.swiftformat --quiet
-cd App && swiftlint autocorrect --quiet
+swiftformat Sources --config ./.swiftformat --quiet
+swiftformat Tests --config ./.swiftformat --quiet
+swiftlint autocorrect --quiet
